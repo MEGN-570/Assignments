5023c01dcf4ac9750ed727faaa1b48c62f6646ab
Rohan Luthra <rohanlluthra.2@gmail.com>
2026-04-02 14:17:25 -0600

    homework 3 updates

--- Luthra/homework3_cantera.py
+++ Luthra/homework3_cantera.py
@@ -5,49 +5,37 @@ import matplotlib.pyplot as plt
 import cantera as ct
 print(f"Running Cantera version: {ct.__version__}")
 
-input_file = "sofc.yaml"
-
-
-F = 96485e3 #C/Kmol
-
-# Temperature and pressure
-T = 300  # K
-P = ct.one_atm
+input_file = "sofc_new.yaml"
 
 gas = ct.Solution(input_file, "gas")
 metal = ct.Solution(input_file, "metal")                    # Ni bulk (anode)
 oxide = ct.Solution(input_file, "oxide_bulk")               # YSZ bulk (cathode)
 metal_surface = ct.Solution(input_file, "metal_surface")    # Ni surface
 oxide_surface = ct.Solution(input_file, "oxide_surface")    # YSZ surface
-tpb = ct.Solution(input_file, "tbp")                        # triple phase boundary
+tpb = ct.Solution(input_file, "tpb")                        # triple phase boundary
+
+R = ct.gas_constant # J/kmol-K
+n = 2               # electrons transferred
+beta = 0.5 
+F = 96485e3         # C/Kmol
+T = 300             # K
+P = ct.one_atm     
 
 phases = [gas, metal, oxide, metal_surface, oxide_surface, tpb]
 for ph in phases:
     ph.TP = T,P
 
-# --- Constants ---
-R = ct.gas_constant  # J/kmol-K
-n = 2                # electrons transferred
-beta = 0.5           # from yaml
-
-# --- Get species indices for TPB reaction ---
-tpb_species = tpb.species_names
-
-i_Hm = tpb_species.index('H(m)')
-i_Oox = tpb_species.index("O''(ox)")
-i_OHm = tpb_species.index('OH(m)')
-i_m = tpb_species.index('(m)')
-
-# --- Get rate constants ---
 k_f = tpb.forward_rate_constants[0]
 k_r = tpb.reverse_rate_constants[0]
 
-# --- Get concentrations ---
-C = tpb.concentrations
+i_Hm = metal_surface.species_names.index("H(m)")
+i_Oox = oxide_surface.species_names.index("O''(ox)")
+i_OHm = metal_surface.species_names.index('OH(m)')
+i_m = metal_surface.species_names.index('(m)')
 
-C_Hm = C[i_Hm]
-C_Oox = C[i_Oox]
-C_OHm = C[i_OHm]
+C_Hm = metal_surface.concentrations[i_Hm]
+C_Oox = oxide_surface.concentrations[i_Oox]
+C_OHm = metal_surface.concentrations[i_OHm]
 
 # --- Compute equilibrium potential difference ---
 # Using mass-action equilibrium condition
@@ -55,7 +43,6 @@ delta_phi_eq = (R * T) / (n * F) * np.log((k_f * C_Hm * C_Oox) / (k_r * C_OHm))
 
 print(f"Delta phi_eq = {delta_phi_eq:.4e} V")
 
-# --- Sweep overpotential ---
 eta = np.linspace(0, 0.3, 100)
 current = []
 
